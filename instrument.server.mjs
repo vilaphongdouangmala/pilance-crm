23f24ab1debd17df5f066467d96c4856a2d568b6
Vilaphong Douangmala <vilaphong.dml@gmail.com>
2025-11-16 22:07:57 +0700

    change env import for instrument.mjs

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -1,6 +1,6 @@
 import * as Sentry from '@sentry/tanstackstart-react'
 Sentry.init({
-  dsn: import.meta.env.VITE_SENTRY_DSN,
+  dsn: process.env.VITE_SENTRY_DSN,
   // Adds request headers and IP for users, for more info visit:
   // https://docs.sentry.io/platforms/javascript/guides/tanstackstart-react/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
